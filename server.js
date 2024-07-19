4d94780db1e8756ac69b9ac1e15d2510e7241ebe
Sateeshkumar B <119934392+zookeeper00@users.noreply.github.com>
2024-07-19 15:56:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const basicAuth = require('basic-auth');
+const basicAuth = require('basic-auth'); // Add this line to import basic-auth
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const app = express();
 const PORT = process.env.PORT || 8080; // Default to 8080 for Cloud Run
 
-// Directly use environment variables set by Cloud Run
-const HUGGING_FACE_API_KEY = process.env.HUGGING_FACE_API_KEY;
-const BASIC_AUTH_USERNAME = process.env.BASIC_AUTH_USERNAME || 'admin';
-const BASIC_AUTH_PASSWORD = process.env.BASIC_AUTH_PASSWORD || 'admin';
+// Initialize Secret Manager client
+const client = new SecretManagerServiceClient();
 
+async function accessSecretVersion(name) {
+  const [version] = await client.accessSecretVersion({
+    name: name,
+  });
+
+  // Extract the secret payload
+  return version.payload.data.toString('utf8');
+}
+
+// Fetch secrets from Secret Manager
+const USERNAME_SECRET = process.env.USERNAME_SECRET;
+const PASSWORD_SECRET = process.env.PASSWORD_SECRET;
+const HUGGING_FACE_API_KEY_SECRET = process.env.HUGGING_FACE_API_KEY_SECRET;
+
+let USERNAME, PASSWORD, HUGGING_FACE_API_KEY;
+
+async function fetchSecrets() {
+  USERNAME = await accessSecretVersion(USERNAME_SECRET);
+  PASSWORD = await accessSecretVersion(PASSWORD_SECRET);
+  HUGGING_FACE_API_KEY = await accessSecretVersion(HUGGING_FACE_API_KEY_SECRET);
+}
+
+fetchSecrets().catch(console.error);
+
+// Authentication middleware
 const auth = (req, res, next) => {
   const user = basicAuth(req);
-
-  if (!user || user.name !== BASIC_AUTH_USERNAME || user.pass !== BASIC_AUTH_PASSWORD) {
+  if (!user || user.name !== USERNAME || user.pass !== PASSWORD) {
     res.set('WWW-Authenticate', 'Basic realm="Restricted area"');
-    res.set('Cache-Control', 'no-store'); // Prevent caching of credentials
     res.status(401).send('Authentication required.');
     return;
   }
-
-  // Prevent caching of authenticated requests
-  res.set('Cache-Control', 'no-store');
-
   // If authentication passes, continue to the next middleware or route handler
   next();
 };
@@ -34,14 +52,12 @@ app.use(express.json());
 
 app.post('/generate-text', async (req, res) => {
   const prompt = req.body.prompt;
-
   try {
     const response = await axios.post(
       'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3',
       { inputs: prompt },
       { headers: { Authorization: `Bearer ${HUGGING_FACE_API_KEY}` } }
     );
-
     res.json({ text: response.data[0].generated_text });
   } catch (error) {
     console.error(error);
